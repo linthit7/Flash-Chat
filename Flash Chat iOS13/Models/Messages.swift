919e85c8d00ae68452c59bfc4bc6b0c3b266807e
starboy711 <teteterrs@gmail.com>
2022-11-24 23:18:31 +0630

    First commit

---
+++ Flash Chat iOS13/Models/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Lin Thit Khant on 9/12/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messages {
+    let sender: String
+    let body: String
+}
